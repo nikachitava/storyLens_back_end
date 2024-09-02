e53d4b165defb7a704a9ac00b3bfdfcbe0f4f97c
nikachitava <chitava18.official@gmail.com>
2024-09-02 23:06:14 +0400

    fix likes query

--- controllers/Likes.js
+++ controllers/Likes.js
@@ -1,48 +1,34 @@
-import jwt from "jsonwebtoken";
 import { connection } from "../connection.js";
 
 export const getLikes = (req,res)=>{
-    const q = "SELECT userID FROM likes WHERE postID = ?";
+    const query = "SELECT userID FROM likes WHERE postID = ?";
 
-    connection.query(q, [req.query.postID], (err, data) => {
+    connection.query(query, [req.query.postID], (err, data) => {
       if (err) return res.status(500).json(err);
       return res.status(200).json(data.map(like=>like.userID));
     });
 }
 
 export const addLike = (req, res) => {
-  const token = req.cookies.accessToken;
-  if (!token) return res.status(401).json("Not logged in!");
 
-  jwt.verify(token, "secretkey", (err, userInfo) => {
-    if (err) return res.status(403).json("Token is not valid!");
-
-    const q = "INSERT INTO likes (`userID`,`postID`) VALUES (?)";
+    const query = "INSERT INTO likes (`userID`,`postID`) VALUES (?)";
     const values = [
-      userInfo.id,
+      req.body.userID,
       req.body.postID
     ];
 
-    connection.query(q, [values], (err, data) => {
+    connection.query(query, [values], (err, data) => {
       if (err) return res.status(500).json(err);
       return res.status(200).json("Post has been liked.");
     });
-  });
 };
 
 export const deleteLike = (req, res) => {
 
-  const token = req.cookies.accessToken;
-  if (!token) return res.status(401).json("Not logged in!");
+    const query = "DELETE FROM likes WHERE `userID` = ? AND `postID` = ?";
 
-  jwt.verify(token, "secretkey", (err, userInfo) => {
-    if (err) return res.status(403).json("Token is not valid!");
-
-    const q = "DELETE FROM likes WHERE `userID` = ? AND `postID` = ?";
-
-    connection.query(q, [userInfo.id, req.query.postID], (err, data) => {
+    connection.query(query, [req.query.userID, req.query.postID], (err, data) => {
       if (err) return res.status(500).json(err);
       return res.status(200).json("Post has been disliked.");
     });
-  });
 };
