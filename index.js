8079ea147c8fe40dd8b9a20b00c270289973d1b1
nikachitava <chitava18.official@gmail.com>
2024-09-06 21:39:14 +0400

    test

--- index.js
+++ index.js
@@ -14,15 +14,16 @@ import commentsRoute from './routes/Comments.js'
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Credentials", true);
-    next();
-});
-// app.use(
-//     cors({
-//         origin: "http://localhost:5173",
-//     })
-// );
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Credentials", true);
+//     next();
+// });
+app.use(
+    cors({
+        origin: "https://story-lens-indol.vercel.app",
+        crede
+    })
+);
 app.use(express.json());
 app.use(cookieParser());
 
