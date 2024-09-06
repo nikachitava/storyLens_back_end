a811848915534df39a628d59209a5be187b2d51d
nikachitava <chitava18.official@gmail.com>
2024-09-06 16:53:01 +0400

    setup for deploy

--- connection.js
+++ connection.js
@@ -1,8 +1,10 @@
 import mysql from 'mysql'
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "the-blog",
-    password: "P@ssw0rd",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
 })
