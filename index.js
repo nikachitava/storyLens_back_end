da9e02ac37415a74f51c145cfa70c0bac98ac76c
nikachitava <chitava18.official@gmail.com>
2024-09-06 17:48:08 +0400

    setup for deplyment

--- index.js
+++ index.js
@@ -18,11 +18,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", true);
     next();
 });
-app.use(
-    cors({
-        origin: "https://story-lens-indol.vercel.app",
-    })
-);
+
 app.use(express.json());
 app.use(cookieParser());
 
