f6475e8e1befc2019c2945b400a633d618028681
nikachitava <chitava18.official@gmail.com>
2024-09-06 21:32:54 +0400

    test

--- index.js
+++ index.js
@@ -18,7 +18,11 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", true);
     next();
 });
-
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+    })
+);
 app.use(express.json());
 app.use(cookieParser());
 
